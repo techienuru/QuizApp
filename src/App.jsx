d2872a699b7134572055cd825367a042ed9554ad
Ibrahim Nurudeen shehu <ibrahimnurudeenshehu1447@gmail.com>
2025-05-08 15:14:07 +0100

    Feat: Initialize React project with structured folders and Bootstrap setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1 className="bg-danger">Quiz App</h1>
+      </header>
+      <main>
+        <h2 className="bg-secondary">Welcome to my Quiz App</h2>
+      </main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
