c418b1768976dd626e2ec33f6b07c87909c38bbf
Ibrahim Nurudeen shehu <ibrahimnurudeenshehu1447@gmail.com>
2025-05-12 18:26:16 +0100

    Import all Pages into App.jsx; only the Home page is not commented

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 // import { useEffect } from "react";
 import Home from "./pages/Home/Home.jsx";
-import PlayOptionsModal from "./components/Header/PlayOptionsModal/PlayOptionsModal.jsx";
+import PlayOptionsModal from "./components/PlayOptionsModal/PlayOptionsModal.jsx";
 import Result from "./pages/Result/Result.jsx";
+import Quiz from "./pages/Quiz/Quiz.jsx";
 // import { useState } from "react";
 
 function App() {
   return (
     <>
-      <Result />
-      {/* <Home /> */}
+      {/* <Result /> */}
+      <Home />
+      {/* <Quiz /> */}
       <PlayOptionsModal />
     </>
   );
