94b9fc08c752eada81f4cc396a1cbe09774622b3
Ibrahim Nurudeen shehu <ibrahimnurudeenshehu1447@gmail.com>
2025-05-14 12:00:24 +0100

    Arrange routing on App.jsx page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-// import { useEffect } from "react";
 import Home from "./pages/Home/Home.jsx";
 import PlayOptionsModal from "./components/PlayOptionsModal/PlayOptionsModal.jsx";
 import Result from "./pages/Result/Result.jsx";
 import Quiz from "./pages/Quiz/Quiz.jsx";
-// import { useState } from "react";
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
+  const [isModalOpen, setisModalOpen] = useState(false);
+
+  const openModal = () => {
+    setisModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setisModalOpen(false);
+  };
+
   return (
     <>
-      {/* <Result /> */}
-      <Home />
-      {/* <Quiz /> */}
-      <PlayOptionsModal />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home openModal={openModal} />}></Route>
+            <Route path="/result" element={<Result />}></Route>
+            <Route
+              path="/quiz/:noOfQuestion/:categoryId/:difficulty/:questionType"
+              element={<Quiz />}
+            ></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      {isModalOpen && <PlayOptionsModal closeModal={closeModal} />}
     </>
   );
 }
